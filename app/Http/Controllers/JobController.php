e899fa21d18ed3a0e5a17950bfe4d04135f7d21d
Ali Sbane <ali.sbane.dev@gmail.com>
2025-02-28 17:34:32 +0000

    [~] Show the latest jobs first

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -14,7 +14,7 @@ class JobController extends Controller
      */
     public function index()
     {
-        $jobs = Job::with("tags", "employer")->get()->groupBy('featured');
+        $jobs = Job::with("tags", "employer")->latest()->get()->groupBy('featured');
 
 
         return view('jobs.index')->with([
