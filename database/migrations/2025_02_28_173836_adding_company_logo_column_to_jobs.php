1f2a2cf3fda475451ef12137d82350e9bfdfd2e6
Ali Sbane <ali.sbane.dev@gmail.com>
2025-03-09 16:58:52 +0000

    [+] Adding the dropping column part for rollbacks :)

--- database/migrations/2025_02_28_173836_adding_company_logo_column_to_jobs.php
+++ database/migrations/2025_02_28_173836_adding_company_logo_column_to_jobs.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('jobs', function (Blueprint $table) {
-            //
+            $table->dropColumn('company_logo')
         });
     }
 };
