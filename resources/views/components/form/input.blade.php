9467797e945524030e5d3518d05e03c93001d949
Ali Sbane <ali.sbane.dev@gmail.com>
2025-03-09 18:06:02 +0000

    [~] With the prev commit :)

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,2 +1,2 @@
 <input
-    {{ $attributes->class('w-full  rounded-md border border-zinc-700 bg-zinc-900 px-4 py-2.5 placeholder:text-sm placeholder:text-zinc-600') }}>
+    {{ $attributes->class('w-full rounded-md border border-zinc-700 bg-zinc-900 px-4 py-2.5 placeholder:text-sm placeholder:text-zinc-600') }}>
